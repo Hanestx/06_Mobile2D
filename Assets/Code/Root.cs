a2fd298aa9ea0826bcea4c7e942dc14f217ca76f
Hanest <53016199+Hanestx@users.noreply.github.com>
2022-08-11 19:29:01 +0400

    Revert "HW_01"
    This reverts commit be5485fbe3be6956eaa64cd10903e5789111c738.

--- Assets/Code/Root.cs
+++ Assets/Code/Root.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Mobile2D
@@ -6,21 +5,15 @@ namespace Mobile2D
     public class Root : MonoBehaviour
     {
         [SerializeField] private Transform _placeForUi;
-        private ProfilePlayer profilePlayer;
+       
         private float _speedCar = 15f;
         private MainController _mainController;
 
         private void Awake()
         {
-            profilePlayer = new ProfilePlayer(_speedCar);
+            ProfilePlayer profilePlayer = new ProfilePlayer(_speedCar);
             profilePlayer.CurrentState.Value = GameState.Start;
             _mainController = new MainController(_placeForUi, profilePlayer);
-        }
-
-        private void Update()
-        {
-            if (profilePlayer.CurrentState.Value == GameState.Start)
-                _mainController.Execute();
         }
 
         private void OnDestroy()
